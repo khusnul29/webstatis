cd08c5e8aec063ab6f40fd7e4cc1134ff9696ce9
khusnul <110731026+khusnul29@users.noreply.github.com>
2022-12-01 19:02:01 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Riwayat</title>
+	<style type="text/css">
+		#main {
+			background-image:('FOTO SMK.jpeg');
+		}
+	</style>
+	  <link href="gambar.css" rel="stylesheet" /> 
+</head>
+<body>
+<div id="main">
+	
+	<p align="center">
+		<br>
+	<h1 align="center"> """""""""""""""""""""""""""""""""""""" </h1>
+	<h3 align="center">========== SELAMAT DATANG ============</h3>
+	<h5 align="center"> || </h5>
+	<section>
+	<p align="center"><img src="foto1.jpg" align="center" height="200" width="200" border="5"></p>
+	</section>
+
+<table style="width: 950px; background-color:#ffffff" align="center" cellpadding="5" cellspacing="0" border="2px">
+ <tr>
+ <td colspan="4" style="text-align: center; background-color: powderblue;color: black"><b>Riwayat Pendidikan </b></td>
+ </tr>
+ <tr>
+ <td style="text-align: center">Nama Siswa</td>
+ <td style="text-align: center">Jenjang Pendidikan</td>
+ <td style="text-align: center">Nama Sekolah</td>
+ <td style="text-align: center">Jurusan</td>
+ <td style="text-align: center">Tahun</td>
+ </tr>
+ <tr>
+ <td>KHUSNUL</td>
+ <td>SMK</td>
+ <td>SMK TELKOM MALANG</td>
+ <td>RPL (REKAYASA PERANGKAT LUNAK)</td>
+ <td>2021 s.d 2022</td>
+ </tr>
+ <tr>
+ <td>ILHAM</td>
+ <td>SMK</td>
+ <td>SMK TELKOM MALANG</td>
+ <td>TKJ (TEKNIK KOMPUTER JARINGAN)</td>
+ <td>2022 s.d 2023</td>
+ </tr>
+ <tr>
+ </table>
+ <br>
+ <table style="width: 750px;">
+ <tr>
+ </table>
+<br>
+<h5 align="center">OKEE</h5>
+			<br>
+			<br>
+
+</p>
+</div>
+</body>
+</html>
